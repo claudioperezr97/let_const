67547469093d3ce0a2167a6070300d5d51be9047
claudioperezr97 <117235760+claudioperezr97@users.noreply.github.com>
2022-11-01 17:33:48 -0600

    Add files via upload
    adding files

---
+++ let_const.js
@@ -0,0 +1,18 @@
+const pi = 3.14
+pi = 40
+/*
+1. What is the difference between var and let?
+
+- Hoisting occurs with var but not with let. Block level scope with let.
+
+2. What is the difference between var and const?
+
+- You cannot redeclare o reassign a const variable.
+
+3. What is the difference between let and const?
+
+- You can reassign with let but you cant reassign with const (or redeclare)
+
+4. What is hoisting?
+
+- A variable is undefined before it is declared because of the way js compiles code. */
